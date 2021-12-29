91b6a2f07ac1b1d28cdfcbaa0b6522f57abc3b1f
Hp <Hp@RIYA>
2021-12-29 09:41:02 +0530

    BY ADM

---
+++ Demo1/src/Test.java
@@ -0,0 +1,15 @@
+
+
+public class Test {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("PRINT HELLO");
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("HELLO");
+		}
+	}
+}
